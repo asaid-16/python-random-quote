0c45fcab34e0b19ddd42239e57bf8b226e6060b4
Athina Said <athinasaid@Athina.local>
2020-12-24 10:31:23 -0500

    Explored

--- get-quote.py
+++ get-quote.py
@@ -6,10 +6,20 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
+  last = len(quotes)
 
+  rnd = random.randint(0, last-1)
   print(quotes[rnd])
+
+  # file-append.py
+  # f = open('quotes.txt', 'a')
+  # f.write('\n' + 'hello AS')
+  # f.close()
+
+  for x in range(0, last, 2):
+    print(quotes[x]),
+
+
 
 if __name__== "__main__":
   primary()
